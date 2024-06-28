bd980fe6ce6a1cf65f222cb0d0809702f9311392
harsh3311 <harshbhardwajofficial1414@gmail.com>
2024-06-28 12:52:35 +0530

    Home/page commit

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,5 +1,7 @@
 import MeetingTypeList from '../../../components/MeetingTypeList';
 import React from 'react'
+import Link from 'next/link'
+import Image from 'next/image';
 
 const Home = () => {
   const now = new Date();
@@ -10,8 +12,8 @@ const Home = () => {
     <section className="flex size-full flex-col gap-10 text-white">
       <div className="h-[300px] w-full rounded-[20px] bg-hero bg-cover">
         <div className="flex h-full flex-col justify-between max-md:px-5 max-md:py-8 lg:p-11">
-          <h2 className="backdrop-blur-sm bg-[#778da9]/50 rounded-md py-2 text-center text-base font-normal text-white-1">
-          Connect, Collaborate, and Capture every moment with <b>quick.link</b> - ultimate video calling platform.
+          <h2 className="backdrop-blur-sm bg-[#778da9]/50 rounded-md py-2 text-center text-base font-normal text-white-1 max-md:px-4 max-md:text-justify">
+            Connect, Collaborate, and Capture every moment with <b>quick.link</b> - ultimate video calling platform.
           </h2>
           <div className="flex flex-col gap-2">
             <h1 className="text-4xl font-extrabold lg:text-6xl">
@@ -24,6 +26,10 @@ const Home = () => {
         </div>
       </div>
       <MeetingTypeList />
+      <div className='flex justify-center text-black-1 font-light gap-2'>
+        <b>Connect :</b>
+        <Link className='flex justify-center' href={'https://www.linkedin.com/in/harsh3311/'}><Image src='/icons/github.svg' width={24} height={24} alt='github logo' /></Link>
+        <Link className='flex justify-center' href={'https://www.github/harsh3311/'}><Image src='/icons/linkedin.svg' width={24} height={24} alt='github logo' /></Link></div>
     </section>
   )
 }
